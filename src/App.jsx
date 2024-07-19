92c900efe096411906e553bfc2b1ff3261d6d0f0
BeingRooted <akhilvinod06@gmail.com>
2024-07-19 19:47:24 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Arrya from './pages/Arrya'
 import Nissy from './pages/Nissy'
+import Akhil from './pages/Akhil'
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Routes>
        <Route path='/' element={<Arrya/>} />
        <Route path='/nissy' element={<Nissy/>} />
+       <Route Path='/' elements={<Akhil/>} />
        {/*<Route path='' element={} /> */}
        {/* <Route path='' element={} /> */}
        {/* <Route path='' element={} /> */}
---
+++ src/pages/Akhil.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Akhil() {
+  return (
+    <>
+    <div>
+        <h1>Akhil</h1>
+    </div>
+    </>
+  )
+}
+
+export default Akhil
